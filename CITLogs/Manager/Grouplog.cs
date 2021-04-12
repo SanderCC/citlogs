08c0f49f7ffee5def7c05bc6da0025069a28691b
Sander Constantin <sander.constantin@student.kdg.be>
2021-04-12 15:42:21 +0200

    Set Custom title as a separate div since most of the Others logs are from there

--- CITLogs/Manager/Grouplog.cs
+++ CITLogs/Manager/Grouplog.cs
@@ -12,6 +12,7 @@ namespace Manager
         public List<string> Warnings { get; set; }
         public List<string> Applications { get; set; }
         public List<string> Management { get; set; }
+        public List<string> CustomTitle { get; set; }
         public List<string> Other { get; set; }
 
         public Grouplog()
@@ -22,6 +23,7 @@ namespace Manager
             Warnings = new List<string>();
             Applications = new List<string>();
             Management = new List<string>();
+            CustomTitle = new List<string>();
             Other = new List<string>();
         }
 
@@ -33,10 +35,8 @@ namespace Manager
                 {
                     if (line.Length < 5) Console.WriteLine("Line too small, skipped.");
                     
-                    else if (line.Contains(") updated the group info")) Management.Add(line);
-                    else if (line.Contains(" has updated group application.")) Management.Add(line);
-                    else if (line.Contains(" blacklist.")) Management.Add(line);
-                    else if (line.Contains("updated the group whitelist")) Management.Add(line);
+                    else if (line.Contains("'s application. (")) Applications.Add(line);
+                    else if (line.Contains(" has submitted an application.")) Applications.Add(line);
                     
                     else if (line.Contains(" is promoting ")) RankChanges.Add(line);
                     else if (line.Contains(" is demoting ")) RankChanges.Add(line);
@@ -45,13 +45,15 @@ namespace Manager
                     else if (line.Contains(" has kicked ")) JoinsDepartures.Add(line);
                     else if (line.Contains(") left the group as ")) JoinsDepartures.Add(line);
                     
-                    else if (line.Contains("'s application. (")) Applications.Add(line);
-                    else if (line.Contains(" has submitted an application.")) Applications.Add(line);
-                    
                     else if (line.Contains(" deposited to ")) Bank.Add(line);
                     else if (line.Contains(" in the group bank")) Bank.Add(line);
                     else if (line.Contains(" group bank for reason:")) Bank.Add(line);
                     else if (line.Contains(" withdrew ")) Bank.Add(line);
+                    
+                    else if (line.Contains(") updated the group info")) Management.Add(line);
+                    else if (line.Contains(" has updated group application.")) Management.Add(line);
+                    else if (line.Contains(" blacklist.")) Management.Add(line);
+                    else if (line.Contains("updated the group whitelist")) Management.Add(line);
                     
                     else if (line.Contains(") warned ")) Warnings.Add(line);
                     else if (line.Contains(") rewarded ")) Warnings.Add(line);
@@ -60,6 +62,8 @@ namespace Manager
                     else if (line.Contains(") updated rank: ")) Management.Add(line);
                     else if(line.Contains(") added rank: ")) Management.Add(line);
                     else if (line.Contains(") removed rank: ")) Management.Add(line);
+                    
+                    else if (line.Contains("custom title")) CustomTitle.Add(line);
                     
                     else Other.Add(line);
                 }
