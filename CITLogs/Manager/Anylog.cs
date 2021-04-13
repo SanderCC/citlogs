3d2d5306b12cadfcbdfaa1f134de9e902ef808c0
Sander Constantin <sander.constantin@student.kdg.be>
2021-04-13 15:58:44 +0200

    Made it possible to have your own filter

---
+++ CITLogs/Manager/Anylog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Manager
+{
+    public class Anylog
+    {
+        public List<string> Filtered { get; set; }
+        public double Duration { get; set; } = 0;
+        public string Error { get; set; } = null;
+        public Anylog()
+        {
+            Filtered = new List<string>();
+        }
+
+        public List<string> Filter(string input, string filterInput)
+        {
+            DateTime start = DateTime.Now;
+            List<string> filters = new List<string>();
+            
+            try
+            {
+                foreach (string filter in filterInput.Split("\n"))
+                {
+                    filters.Add(filter);
+                }
+                
+                foreach (string line in input.Split("\n"))
+                {
+                    foreach (string filter in filters)
+                    {
+                        if(line.Contains(filter)) Filtered.Add(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+            }
+            Duration = (DateTime.Now - start).TotalSeconds;
+            return Filtered;
+        }
+    }
+}
